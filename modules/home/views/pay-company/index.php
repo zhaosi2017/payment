8843d90b49ef87fbadfd7c523e978ee00ef4b4d7
zhaosi2017 <zhaosi2017@gmail.com>
2017-06-27 10:46:21 +0700

    up

--- modules/home/views/pay-company/index.php
+++ modules/home/views/pay-company/index.php
@@ -33,7 +33,7 @@ $this->params['breadcrumbs'][] = $this->title;
         ],
         'columns' => [
 //            ['class' => 'yii\grid\SerialColumn'],
-
+            ['class' => 'yii\grid\SerialColumn','header' => '序号'],
             'id',
             'pay_plate_name',
             'name:ntext',
