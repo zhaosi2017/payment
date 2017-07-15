7f944265ec86d15738daa8c75e5428eaf5f1cdf8
zhaosi2017 <zhaosi2017@gmail.com>
2017-07-15 15:36:22 +0700

    set db config file

--- config/dbback.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=43.229.38.236;dbname=payment',
-    'username' => 'root',
-    'password' => 'OASystem@Yii2+H+',
-    'charset' => 'utf8',
-];
