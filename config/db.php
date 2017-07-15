f7597a984b8e563d8eb8ffc050980994a5c3386d
root <root@localhost.localdomain>
2017-07-15 15:30:10 -0400

    db

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=payment',
+    'dsn' => 'mysql:host=43.229.38.236;dbname=payment',
     'username' => 'root',
     'password' => 'OASystem@Yii2+H+',
     'charset' => 'utf8',
